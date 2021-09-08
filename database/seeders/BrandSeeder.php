13ee2d87a4898235da3962ac334a94a93662bb1d
Rizal Taufiq <45067904+Pakrt@users.noreply.github.com>
2021-09-08 22:23:34 +0700

    asd

---
+++ database/seeders/BrandSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BrandSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('brands')->insert([
+            [
+                'id' => '1',
+                'name' => 'APPLE',
+                'created_at' => date("Y-m-d h:i:s"),
+                'updated_at' => date("Y-m-d h:i:s"),
+            ],
+            [
+                'id' => '2',
+                'name' => 'REALME',
+                'created_at' => date("Y-m-d h:i:s"),
+                'updated_at' => date("Y-m-d h:i:s"),
+            ],
+        ]);
+    }
+}
