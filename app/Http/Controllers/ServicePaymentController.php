5a31fa72bcdb3e0b88b0b91e6b261f62afaa7fd3
Azriel <denyprasetyo41@gmail.com>
2021-10-07 20:43:44 +0700

    Update ServicePaymentController.php

--- app/Http/Controllers/ServicePaymentController.php
+++ app/Http/Controllers/ServicePaymentController.php
@@ -464,7 +464,7 @@ class ServicePaymentController extends Controller
     }
     public function printServicePayment($id)
     {
-        return $service = Service::with('ServiceDetail','ServiceDetail.Items','Employee1','Employee2','CreatedByUser','Type','Brand','Brand.Category','ServiceEquipment','ServiceCondition')->find($id);
+        return $service = ServicePayment::with('Service','Service.ServiceDetail','Service.ServiceDetail.Items','Service.Employee1','Service.Employee2','Service.CreatedByUser','Service.Type','Service.Brand','Service.Brand.Category','Service.ServiceEquipment','Service.ServiceCondition')->find($id);
         // return $Service;
         $member = User::get();
         return view('pages.backend.transaction.servicePayment.printServicePayment', ['service' => $service,'member'=>$member]);
