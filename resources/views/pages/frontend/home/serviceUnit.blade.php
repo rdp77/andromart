ce4607354f7c758adce3618f97a1cdbe9659a20e
Azriel <denyprasetyo41@gmail.com>
2021-11-26 01:55:02 +0700

    Update serviceUnit.blade.php

--- resources/views/pages/frontend/home/serviceUnit.blade.php
+++ resources/views/pages/frontend/home/serviceUnit.blade.php
@@ -42,6 +42,18 @@
     </div>
 </section>
 <script type="text/javascript">
+	var input = document.getElementById("nameText");
+
+	// Execute a function when the user releases a key on the keyboard
+	input.addEventListener("keyup", function(event) {
+	// Number 13 is the "Enter" key on the keyboard
+	if (event.keyCode === 13) {
+		// Cancel the default action, if needed
+		event.preventDefault();
+		// Trigger the button element with a click
+		show();
+	}
+	});
     function show() {
     	var serviceId = document.getElementById("nameText").value;
     	var urls = /trackingService/ + serviceId;
