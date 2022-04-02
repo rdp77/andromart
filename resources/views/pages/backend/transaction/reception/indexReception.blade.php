6b53c1689f344c88ae7fd7eed4064785a4467a83
Alfian Arifianto <alfianarifianto666@gmail.com>
2022-04-03 04:53:34 +0700

    jurnal penerimaan

--- resources/views/pages/backend/transaction/reception/indexReception.blade.php
+++ resources/views/pages/backend/transaction/reception/indexReception.blade.php
@@ -32,6 +32,51 @@
         </table>
     </div>
 </div>
+
+<div class="modal  fade" tabindex="-1" role="dialog" id="exampleModal" data-backdrop="false">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                {{-- <div class="dropHereJournals"> --}}
+                    <h6>Jurnal Pendapatan</h6>
+                    <table class="table table-stripped table-bordered">
+                        <tr>
+                            <th>Kode</th>
+                            <th>Nama</th>
+                            <th>D</th>
+                            <th>K</th>
+                        </tr>
+                        <tbody class="dropHereJournals">
+
+                        </tbody>
+                    </table>
+
+                    <h6>Jurnal HPP</h6>
+                    <table class="table table-stripped table-bordered">
+                        <tr>
+                            <th>Kode</th>
+                            <th>Nama</th>
+                            <th>D</th>
+                            <th>K</th>
+                        </tr>
+                        <tbody class="dropHereJournalsHpp">
+
+                        </tbody>
+                    </table>
+                {{-- </div> --}}
+            </div>
+            <div class="modal-footer bg-whitesmoke br">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
 @section('script')
 <script src="{{ asset('assets/pages/transaction/receptionScript.js') }}"></script>
