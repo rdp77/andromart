9cd7c676dd630dbddd8b5e153f3a3e496540e9cb
Azriel <denyprasetyo41@gmail.com>
2022-04-22 20:52:38 +0700

    Update ReportSummaryController.php

--- app/Http/Controllers/ReportSummaryController.php
+++ app/Http/Controllers/ReportSummaryController.php
@@ -103,7 +103,7 @@ class ReportSummaryController extends Controller
                         if ($data[$i]->JournalDetail[$j]->AccountData->main_detail_id == '30') {
                             $totalDiskonPenjualan += $data[$i]->JournalDetail[$j]->total;
                         }
-                        if ($data[$i]->JournalDetail[$j]->AccountData->main_detail_id == '11' && $data[$i]->JournalDetail[$j]->debet_kredit == 'D') {
+                        if ($data[$i]->JournalDetail[$j]->AccountData->main_detail_id == '12' && $data[$i]->JournalDetail[$j]->debet_kredit == 'D') {
                             $totalPembelian += $data[$i]->JournalDetail[$j]->total;
                             array_push( $tempPembelian,$data[$i]->JournalDetail[$j]);
                         }
@@ -124,7 +124,7 @@ class ReportSummaryController extends Controller
                         if ($data[$i]->JournalDetail[$j]->AccountData->main_detail_id == '30') {
                             $totalDiskonPenjualan += $data[$i]->JournalDetail[$j]->total;
                         }
-                        if ($data[$i]->JournalDetail[$j]->AccountData->main_detail_id == '11' && $data[$i]->JournalDetail[$j]->debet_kredit == 'D') {
+                        if ($data[$i]->JournalDetail[$j]->AccountData->main_detail_id == '12' && $data[$i]->JournalDetail[$j]->debet_kredit == 'D') {
                             $totalPembelian += $data[$i]->JournalDetail[$j]->total;
                             array_push( $tempPembelian,$data[$i]->JournalDetail[$j]);
                            
